4b4028316a517a6878290d0e4f6ebce85ca0932a
Foebry <rain_fabry@hotmail.com>
2021-03-09 16:15:34 +0100

    Created database functionality for mySQL database with testcases

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup 
-setup( 
-    name = 'Databases', 
-    version = 1.0.0, 
-    packages = [], 
-    url = 'https://github.com/Foebry/Databases', 
-    author = 'Foebry', 
-    author_email = 'rain_fabry@hotmail.com', 
-    description = '' 
-) 
+from setuptools import setup
+setup(
+    name = 'Databases',
+    version = 1.0.0,
+    packages = ['mysql-connector'],
+    requirements = ['git+https://github.com/Foebry/Logger.git#egg=Logger'],
+    url = 'https://github.com/Foebry/Databases',
+    author = 'Foebry',
+    author_email = 'rain_fabry@hotmail.com',
+    description = ''
+)
