6451356d1b06fc156717be175b4af7eede1e8dda
Sander <71063294+Foebry@users.noreply.github.com>
2021-03-09 16:34:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 setup(
     name = 'Databases',
     version = 1.0,
-    packages=[],
+    packages=['mysql'],
     requirements = [
         'git+https://github.com/Foebry/Logger.git#egg=Logger',
-        'mysql-connector'],
+        ],
     url = 'https://github.com/Foebry/Databases',
     author = 'Foebry',
     author_email = 'rain_fabry@hotmail.com',
