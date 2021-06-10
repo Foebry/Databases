0bb8739a091c817ddd102e4adfd6eb68631e7087
Foebry <rain_fabry@hotmail.com>
2021-06-10 17:36:02 +0200

    Facilitated access to test schema

--- databases/Database.py
+++ databases/Database.py
@@ -3,11 +3,11 @@ import sys
 
 
 class Database:
-    def __new__(self, data, logger):
+    def __new__(self, data, logger, test=False):
         if data["type"] == "MySQL":
 
             try: import mysql
             except: os.system("pip install mysql-connector")
 
             from databases.SQL import SQL
-            return SQL(data, logger)
+            return SQL(data, logger, test)
